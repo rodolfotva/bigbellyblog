36970e303e2681038144f4c7af6ddfdbb402abc4
Rodolfo Alquezar <rodolfotva@gmail.com>
2018-11-29 15:02:41 -0500

    new project structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# bigbellyblog
+# for run the project
+# 1 - mvn clean install tomcat7:run
+# 2 - open the browser and http://localhost:8082/
---
+++ src/main/java/com/tva/bigbellyblog/service/HeaderService.java
@@ -0,0 +1,10 @@
+package com.tva.bigbellyblog.service;
+
+import java.util.List;
+import com.tva.bigbellyblog.model.Header;
+
+public interface HeaderService {
+
+  public List<Header> getAllHeaders();
+
+}
