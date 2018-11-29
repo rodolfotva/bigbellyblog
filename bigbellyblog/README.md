612a6f6303ff9cc9365d47c7546ed2788ecd4f5d
Rodolfo Alquezar <rodolfotva@gmail.com>
2018-11-29 15:01:39 -0500

    deleting wrong commit

--- bigbellyblog/README.md
+++
@@ -1,3 +0,0 @@
-# for run the project
-# 1 - mvn clean install tomcat7:run
-# 2 - open the browser and http://localhost:8082/
--- bigbellyblog/src/main/webapp/static/js/filters/main-filters.js
+++
@@ -1,5 +0,0 @@
-angular.module('main').filter('searchDescFilter', function () {
-	return function (input, query) {
-		return input.replace(RegExp('('+ query + ')', 'gi'), '<span class="search-filter">$1</span>');
-    }
-});
